32b61ea06aab55c5c7e026bd84d9968d6c28b17e
hassan.prog <alielsayed1112@gmail.com>
2021-04-06 02:51:12 +0200

    first commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    public float restartDelay = 1.5f;
+    public GameObject completeLevelUI;
+    public GameObject gameOverUI;
+
+    public void GameOver()
+    {
+        gameOverUI.SetActive(true);
+    }
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
+    public void EndGame()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("GameOVER!");
+            Invoke("Restart", restartDelay);
+            
+        }
+        
+    }
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewayForce = 500f;
+    public PlayerMovement movement;
+    public GameManager gameManage;
+
+
+    private void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+            gameManage.GameOver();
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime, ForceMode.VelocityChange);
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (rb.position.y < -0.5 || rb.position.y > 2)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+            StopForce();
+            gameManage.GameOver();
+        }
+    }
+    void StopForce()
+    {
+        forwardForce = 0f;
+    }
+    
+
+}
